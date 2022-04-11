c45a7c6ff9c4a9a61f2334d6951dc30207bfdb46
Ch. Damsky <demel41@web.de>
2022-04-11 11:45:04 +0200

    seconds2duration - fix

--- common.php
+++ common.php
@@ -338,11 +338,11 @@ trait StubsCommonLib
     private function seconds2duration(int $sec)
     {
         $duration = '';
-        if ($sec > 3600) {
+        if ($sec >= 3600) {
             $duration .= sprintf('%dh', floor($sec / 3600));
             $sec = $sec % 3600;
         }
-        if ($sec > 60) {
+        if ($sec >= 60) {
             $duration .= sprintf('%dm', floor($sec / 60));
             $sec = $sec % 60;
         }
